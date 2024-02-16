6ebb41d8375bb17bb1f1c954fc9f0a5e4fd6c909
Jerry Sandhu <jsa2282@wgu.edu>
2024-02-15 16:40:18 -0800

    added hashtable class

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-#Jerry Sandhu
-#Program to 
+# Jerry Sandhu
+# Program to find the shortest route for a delivery truck
 
 import csv
 import datetime
 
+# This will load the required CSV files containing data
+with open("Data/address.csv") as CSV_address:
+    AddressCSV = csv.reader(CSV_address)
+    AddressCSV = list(address)
+with open("Data/distance.csv") as CSV_distance:
+    DistanceCSV = csv.reader(CSV_distance)
+    DistanceCSV = list(distance)   
