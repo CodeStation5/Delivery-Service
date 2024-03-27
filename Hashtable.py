4799cfe9e8cbb5cf06620661d714dd8368a12c4e
Jerry Sandhu <jsa2282@wgu.edu>
2024-03-27 01:10:06 -0700

    Add wgu source code for hash table

---
+++ Hashtable.py
@@ -0,0 +1,53 @@
+# Class to create the hash table
+# Source code in lines 4 to 54 taken from WGU code: W-1_ChainingHashTable_zyBooks_Key-Value.py
+
+class HashTable:
+    def __init__(self, initial_capacity=30):
+        self.table = []
+        for i in range(initial_capacity):
+            self.table.append([])
+
+    # Update an existing item in the list and add in a new item
+    def insert(self, key, item):
+        # get the bucket list where this item will go.
+        bucket = hash(key) % len(self.table)
+        bucket_list = self.table[bucket]
+
+        # If key is already in the bucket_list then update the key
+        for kv in bucket_list:
+            # print (key_value)
+            if kv[0] == key:
+                kv[1] = item
+                return True
+
+        # if not, insert the item to the end of the bucket list.
+        key_value = [key, item]
+        bucket_list.append(key_value)
+        return True
+
+    # Searches for an item with matching key in the hash table.
+    # Returns the item if found, or None if not found.
+    def search(self, key):
+        # get the bucket list where this key would be.
+        bucket = hash(key) % len(self.table)
+        bucket_list = self.table[bucket]
+        # print(bucket_list)
+
+        # search for the key in the bucket list
+        for kv in bucket_list:
+            # print (key_value)
+            if kv[0] == key:
+                return kv[1]  # value
+        return None
+
+    # Removes an item with matching key from the hash table.
+    def remove(self, key):
+        # get the bucket list where this item will be removed from.
+        bucket = hash(key) % len(self.table)
+        bucket_list = self.table[bucket]
+
+        # remove the item from the bucket list if it is present.
+        for kv in bucket_list:
+            # print (key_value)
+            if kv[0] == key:
+                bucket_list.remove([kv[0], kv[1]])
